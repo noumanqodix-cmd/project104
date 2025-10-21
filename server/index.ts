c24a3ee5c19d78f794a8f6e876461dfd4b97673a
qodixdev <salman248shah@gmail.com>
2025-10-21 16:47:52 +0500

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -3,8 +3,11 @@ import express, { type Request, Response, NextFunction } from "express";
 import cors from "cors";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import dotenv from "dotenv";
+
 
 const app = express();
+dotenv.config();
 
 app.use(cors({
   origin: true,
