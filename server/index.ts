e96d092350a3058ac192a60d8e4756ed7e123d11
qodixdev <salman248shah@gmail.com>
2025-10-21 17:06:56 +0500

    5005 port

--- server/index.ts
+++ server/index.ts
@@ -73,11 +73,11 @@ app.get('/favicon.ico', (req, res) => {
   }
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Other ports are firewalled. Default to 5000 if not specified.
+  // Other ports are firewalled. Default to 5005 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen(port, '127.0.0.1', () => {
+  const port = parseInt(process.env.PORT || '5005', 10);
+  server.listen(port, '0.0.0.0', () => {
     log(`serving on port ${port}`);
   });
 })();
